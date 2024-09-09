a2607bbf9e1dbc84a054ee6cb5199c94404b21cc
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-09-09 07:55:41 +0330

    add sign-up and sign-in using clerk

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,25 +1,50 @@
+import { useSignIn } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
-import { useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  const onSignInPress = () => {};
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) return;
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(root)/(tabs)/home");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling for more info on error handling
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
+      }
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
+    }
+  }, [isLoaded, form]);
 
   return (
-    <ScrollView className="border border-black flex-1 bg-white">
+    <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-8 left-6">
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
             Welcome 👋
           </Text>
         </View>
@@ -27,17 +52,20 @@ const SignIn = () => {
         <View className="p-5">
           <InputField
             label="Email"
-            placeholder="Enter your email"
+            placeholder="Enter email"
             icon={icons.email}
+            textContentType="emailAddress"
             value={form.email}
             onChangeText={(value) => setForm({ ...form, email: value })}
           />
+
           <InputField
             label="Password"
-            placeholder="Enter your password"
+            placeholder="Enter password"
             icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
             value={form.password}
-            secureTextEntry
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
 
@@ -47,19 +75,16 @@ const SignIn = () => {
             className="mt-6"
           />
 
-          {/* OAuth */}
           <OAuth />
 
           <Link
-            href={"/sign-up"}
+            href="/sign-up"
             className="text-lg text-center text-general-200 mt-10"
           >
-            <Text>Don’t have an account? </Text>
+            Don't have an account?{" "}
             <Text className="text-primary-500">Sign Up</Text>
           </Link>
         </View>
-
-        {/* Verification Modal */}
       </View>
     </ScrollView>
   );
